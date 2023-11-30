31643a5cc0570ad1ff00453e02b0c3d6f2e0a738
Daniel <danielcallejas3@gmail.com>
2023-11-29 21:01:16 -0500

    commit 12

---
+++ Controllers/reporteEventoController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoWebFinal.Models;
+
+namespace ProyectoWebFinal.Controllers
+{
+    public class reporteEventoController : Controller
+    {
+        proyectowebEntities bddatos = new proyectowebEntities();
+        // GET: reporteEvento
+        public ActionResult vistaEventos()
+        {
+            return View(bddatos.estadisticas_evento.ToList());
+        }
+    }
+}
